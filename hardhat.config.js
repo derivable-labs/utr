1c4d49b51d90093b776907113531e810ce335f0d
zergity <zergity@gmail.com>
2023-01-06 11:56:56 +0700

    hardhat config and optimizer

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,11 +2,7 @@
 const dotenv = require("dotenv");
 dotenv.config({ path: __dirname + "/.env" });
 
-require("@nomiclabs/hardhat-ethers");
-require("@nomiclabs/hardhat-etherscan");
 require("hardhat-contract-sizer");
-require("@openzeppelin/hardhat-upgrades");
-require("hardhat-gas-reporter");
 
 module.exports = {
     defaultNetwork: 'hardhat',
@@ -17,64 +13,11 @@ module.exports = {
                 settings: {
                     optimizer: {
                         enabled: true,
-                        runs: 1000,
-                    },
-                },
-            },
-            {
-                version: "0.6.6",
-                settings: {
-                    optimizer: {
-                        enabled: true,
-                        runs: 1000,
+                        runs: 4294967295,
                     },
                 },
             }
         ]
-    },
-    networks: {
-        hardhat: {
-            accounts: [
-                {
-                    privateKey: "0x28d1bfbbafe9d1d4f5a11c3c16ab6bf9084de48d99fbac4058bdfa3c80b2908c",
-                    balance: "900000000000000000000000000000000000",
-                },
-                {
-                    privateKey: '0x28d1bfbbafe9d1d4f5a11c3c16ab6bf9084de48d99fbac4058bdfa3c80b2908d',
-                    balance: "900000000000000000000000000000000000",
-                },
-                {
-                    privateKey: '0x0000000000000000000000000000000000000000000000000000000000000001',
-                    balance: "1000000000000000000000000",
-                },
-            ]
-        },
-        mainnet: {
-            url: process.env.BSC_MAINNET_PROVIDER ?? 'https://bsc-dataseed3.binance.org/',
-            accounts: [
-                process.env.MAINNET_DEPLOYER ?? '0x0000000000000000000000000000000000000000000000000000000000000001',
-            ],
-            timeout: 900000,
-            chainId: 56
-        },
-        testnet: {
-            url: process.env.BSC_TESTNET_PROVIDER ?? '',
-            accounts: [
-                process.env.TESTNET_DEPLOYER ?? '0x0000000000000000000000000000000000000000000000000000000000000001',
-            ],
-            timeout: 20000,
-            chainId: 97
-        },
-        localhost: {
-            url: "http://127.0.0.1:8545",
-            accounts: [
-                process.env.LOCAL_DEPLOYER ?? '0x28d1bfbbafe9d1d4f5a11c3c16ab6bf9084de48d99fbac4058bdfa3c80b2908c',
-                '0x0000000000000000000000000000000000000000000000000000000000000001'
-            ]
-        }
-    },
-    etherscan: {
-        apiKey: process.env.ETHERSCAN_API_KEY,
     },
     contractSizer: {
         alphaSort: true,
