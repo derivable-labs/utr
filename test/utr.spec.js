af6dd24e6727262a0a01dc801097bf0439743892
Zergity <zergity@gmail.com>
2023-01-31 15:25:51 +0700

    update constants for UT

--- test/utr.spec.js
+++ test/utr.spec.js
@@ -18,19 +18,18 @@ const scenarios = [
     { fixture: scenario01, fixtureName: "(ETH = 1500 BUSD)" },
 ];
 
-const TRANSFER_FROM_SENDER     = 0;
-const TRANSFER_FROM_ROUTER     = 1;
-const TRANSFER_CALL_VALUE     = 2;
-const ALLOWANCE_CALLBACK  = 0x100;
-const ALLOWANCE_BRIDGE    = 0x200;
-const AMOUNT_EXACT      = 0;
-const AMOUNT_ALL        = 1;
-const EIP_ETH           = 0;
+const TRANSFER_FROM_SENDER  = 0;
+const TRANSFER_FROM_ROUTER  = 1;
+const TRANSFER_CALL_VALUE   = 2;
+const ALLOWANCE_CALLBACK    = 0x100;
+const ALLOWANCE_BRIDGE      = 0x200;
+const AMOUNT_EXACT          = 0;
+const AMOUNT_ALL            = 1;
+const EIP_ETH               = 0;
 const ID_721_ALL = ethers.utils.keccak256(ethers.utils.toUtf8Bytes("UniversalTokenRouter.ID_721_ALL"))
-const ACTION_IGNORE_ERROR               = 1;
-const ACTION_RECORD_CALL_RESULT        = 2;
-const ACTION_INJECT_CALL_RESULT        = 4;
-const ACTION_FORWARD_CALLBACK           = 8;
+const ACTION_IGNORE_ERROR       = 1;
+const ACTION_RECORD_CALL_RESULT = 2;
+const ACTION_INJECT_CALL_RESULT = 4;
 
 scenarios.forEach(function (scenario) {
     describe("Pool Info: " + scenario.fixtureName, function () {
