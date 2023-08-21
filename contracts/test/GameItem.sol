21a9300bfbb07f05876426fa10eb7e7ac7147b4c
BerlinP <bknghi95@gmail.com>
2023-08-21 20:08:00 +0700

    pragma

--- contracts/test/GameItem.sol
+++ contracts/test/GameItem.sol
@@ -1,6 +1,6 @@
 // contracts/GameItem.sol
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.8.0;
+pragma solidity 0.8.20;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
